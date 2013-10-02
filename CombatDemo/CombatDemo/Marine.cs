4f1a559bb63bf02fd92219f0f98a444eefce5bc3
Cory Wilson <sw15338@gmail.com>
2013-10-02 19:19:56 -0400

    Added a Main
    Then I had the program read a line.

--- CombatDemo/CombatDemo/Marine.cs
+++ CombatDemo/CombatDemo/Marine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CombatDemo
 {
-    public class Class1
+    class Marine
     {
     }
 }
---
+++ CombatDemo/CombatDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatDemo
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is now a Console Application Fucker");
+
+            Console.ReadLine();
+        }
+
+    }
+}
